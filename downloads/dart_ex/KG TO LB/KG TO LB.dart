46ba0a0accb16abedba7a184a0b292ac501deae2
s40523147 <40523147@gm.nfu.edu.tw>
2020-01-02 10:24:47 +0800

    修改w17 exam1：重量轉換程式

--- downloads/dart_ex/KG TO LB/KG TO LB.dart
+++ downloads/dart_ex/KG TO LB/KG TO LB.dart
@@ -1,20 +1,26 @@
-//在此輸入重量
-List candf = ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
-
-void main() {
-  for (var i in candf) {
-    if (i[2] == "KG") {
-      var o = (i[0] + i[1]);
-      var kg = num.parse(o);
-      num lb = kg * 0.45359237;
-      
-      print("KG $kg °等於LB $lb°");
-    if (i[2] == "LB") {
-      var o = (i[0] + i[1]);
-      var lb = num.parse(o);
-      num kg = lb * 1 / 0.45359237;
-      print("KG $kg °等於LB $lb°");
-    }
-  }
- }
+KGtoLB(num k){
+return k* 2.2046226218;
 }
+LBtoKG(num l){
+return l*1/ 2.2046226218;
+}
+main() {
+    int len;
+    var type;
+    var number;
+  List temp = ["25k", "30l", "56l", "14k", "68l", "198k"];
+    for (var data in temp) {
+        len = data.length;
+    type = data[len-1];
+    number = data.substring(0, len-1);
+    number = int.parse(number);
+    if (type == "KG"){
+      //print("KG: $type, $number");
+      print("公斤 $number 公斤 = 英鎊 ${KGtoLB(number)} 英鎊");
+    }else{
+      //print("LB: $type, $number");
+      print("英鎊 $number 英鎊 = 公斤 ${LBtoKG(number)} 公斤");
+    }
+      
+  } // for
+} // main
